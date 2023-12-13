3e3c843f8fd2f7f06b169e765f64eb793e2cccad
Polina <51910779+polinkiya@users.noreply.github.com>
2023-12-13 11:25:48 +0300

    Update create_roles.sql

--- create_roles.sql
+++ create_roles.sql
@@ -1,17 +1,21 @@
--- Create the role
 CREATE ROLE administrator;
 
--- Grant the privileges to the role for the "clients" table
 GRANT SELECT, INSERT, UPDATE ON TABLE clients TO administrator;
 GRANT SELECT, INSERT, UPDATE ON TABLE pass TO administrator;
 GRANT SELECT, INSERT, UPDATE ON TABLE contract TO administrator;
 GRANT SELECT, INSERT, UPDATE ON TABLE pass_status TO administrator;
 GRANT SELECT, INSERT, UPDATE ON TABLE type_tariff TO administrator;
 
--- Create the role
 CREATE ROLE director;
-
--- Grant SELECT privileges on all tables in the "public" schema
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO director;
 
+CREATE ROLE sales_department;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE clients, pass TO sales_department;
+GRANT SELECT, INSERT, UPDATE ON TABLE receipts, tariffs, type_tariff, pass_status, activity_status TO sales_department;
+GRANT SELECT, INSERT ON TABLE contract TO sales_department;
+GRANT SELECT ON TABLE attendance TO sales_department;
 
+CREATE ROLE manager;
+GRANT SELECT, INSERT, UPDATE ON TABLE clients, pass, pass_status, activity_status, contract 
+TO manager;
+GRANT SELECT ON TABLE attendance, tariffs, type_tariff, receipts TO manager;
